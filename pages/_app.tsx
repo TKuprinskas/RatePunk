f8b1a7fc2e22402a9244c0e314423f31f66577a1
Tomas Kuprinskas <t.kuprinskas@gmail.com>
2023-06-13 20:54:37 +0300

    variables and global stylings added

---
+++ pages/_app.tsx
@@ -0,0 +1,29 @@
+import '@/styles/index.scss';
+import { Noto_Sans, Caveat_Brush } from 'next/font/google';
+import type { AppProps } from 'next/app';
+
+const notoSans = Noto_Sans({
+  display: 'swap',
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
+
+const caveatBrush = Caveat_Brush({
+  display: 'swap',
+  subsets: ['latin'],
+  weight: ['400'],
+});
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <style jsx global>{`
+        :root {
+          --notoSans-font: ${notoSans.style.fontFamily};
+          --caveatBrush-font: ${caveatBrush.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />;
+    </>
+  );
+}
